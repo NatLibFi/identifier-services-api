d02f88008a0ca579a52dc9db4e854a4114ac89f5
Codefeak <codefever17@gmail.com>
2020-05-27 12:34:40 +0300

    Debug publisher-admin unable to view users information related to it (#85)
    * publisher-admin user debug
    
    * publisher-admin user debug
    
    * publisher-admin user debug
    
    * publisher-admin user debug
    
    * publisher-admin user debug
    
    * publisher-admin user debug
    
    Co-authored-by: Amatya Rojak <ramatya@lx9-9815-004.lib.helsinki.fi>

--- src/userProvider/crowd.js
+++ src/userProvider/crowd.js
@@ -132,12 +132,11 @@ export default function ({CROWD_URL, CROWD_APP_NAME, CROWD_APP_PASSWORD, PRIVATE
     const response = await userMetadataInterface.read(db, id);
     const {crowdUser} = crowd();
     const result = await crowdUser.read({id: response.userId ? response.userId : response.id});
-
     if (hasPermission(user, 'users', 'read')) {
       // Need to filter user information after combining and before returning to clientSide
       const filteredDoc = filterDoc(result);
       if (user.role === 'publisher-admin') {
-        if (user.id === filteredDoc.publisher || user.id === filteredDoc.id) {
+        if (user.id === response.publisher || user.id === response.id) {
           return {...response, ...filteredDoc};
         }
         throw new ApiError(HttpStatus.UNAUTHORIZED);
